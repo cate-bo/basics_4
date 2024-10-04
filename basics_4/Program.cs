a0e5963ec9a56b5599dfc483231459e6b9b1e793
cate <cate.boehm@oebb.at>
2024-10-04 07:40:43 +0200

    uahfghiusfuiguidf

---
+++ basics_4/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace basics_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int testInt = 500;
+            short testShort = 100;
+            float testfloat = 3.5f;
+
+            int targetInt = 0;
+            short targetShort = 0;
+
+            //implicit casting
+            targetInt = testShort;
+
+            //targetShort = testInt;
+
+
+
+            //explicit cast
+            targetShort = (short)testInt;
+
+
+
+
+
+
+        }
+    }
+}
