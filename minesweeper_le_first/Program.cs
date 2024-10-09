38bbf9dfbc1c71183497578c3d6543c36c9addce
cate <cate.boehm@oebb.at>
2024-10-09 11:21:12 +0200

    game working ...... mostly

--- minesweeper_le_first/Program.cs
+++ minesweeper_le_first/Program.cs
@@ -2,7 +2,7 @@
 {
     internal class Program
     {
-        
+
         struct Fieldstate
         {
             public byte adjacentMines = 0;
@@ -14,18 +14,23 @@
             {
             }
         }
-        
+
         static int areaWidth = 0;
         static int areaHeight = 0;
         static int mineCount = 0;
         static Fieldstate[,] playArea; //0-8 adjacent mines, 9 mine 
         static Random rand = new Random();
+        static bool gameInProgress;
+        static bool lose = false;
+        static bool win = false;
         static void Main(string[] args)
         {
-            
 
+            bool playAgain = false;
             do
             {
+                Console.Clear();
+                gameInProgress = true;
                 Console.WriteLine("enter area width");
                 areaWidth = Validate_Input(2, int.MaxValue);
 
@@ -46,6 +51,8 @@
                     }
                 }
                 */
+                win = false;
+                lose = false;
                 while (mineCount > 0)
                 {
                     int x = rand.Next(0, playArea.GetLength(0));
@@ -57,88 +64,192 @@
                     }
                 }
 
-                DisplayPlayArea();
+                //DisplayPlayArea();
                 CountAdjacentMines();
                 Console.WriteLine();
                 DisplayPlayArea();
 
+                while (gameInProgress)
+                {
+                    Console.WriteLine("select column");
+                    int inputX = Validate_Input(0, playArea.GetLength(0));
+                    Console.WriteLine("select rank");
+                    int inputY = Validate_Input(0, playArea.GetLength(1));
+                    Console.WriteLine("select action reveal or flag (R/f)\n");
+                    while (true)
+                    {
+                        ConsoleKey input = Console.ReadKey().Key;
 
-            } while (true);
+                        if (input == ConsoleKey.R || input == ConsoleKey.Enter)
+                        {
+                            RevealField(inputX, inputY);
+                            break;
+                        }
+                        else if (input == ConsoleKey.F)
+                        {
+                            PlaceFlag(inputX, inputY);
+                            break;
+                        }
+                    }
+                    Console.WriteLine("\n");
+
+                    DisplayPlayArea();
+                    CheckIfWon();
+                    if (win || lose) break;
+
+
+
+
+
+                }
+
+                if (win)
+                {
+                    Console.WriteLine("you win\n");
+
+                    RevealAllFields();
+                    DisplayPlayArea();
+                }
+                else if (lose)
+                {
+                    Console.WriteLine("you lose\n");
+
+                    RevealAllFields();
+                    DisplayPlayArea();
+                }
+                else
+                {
+                    Console.WriteLine("program broke");
+                }
+
+                Console.WriteLine("play again?(Y/n)\n");
+                while (true)
+                {
+                    ConsoleKey input = Console.ReadKey().Key;
+
+                    if (input == ConsoleKey.Y || input == ConsoleKey.Enter)
+                    {
+                        playAgain = true;
+                        break;
+                    }
+                    else if (input == ConsoleKey.N)
+                    {
+                        playAgain = false;
+                        break;
+                    }
+                }
+
+            } while (playAgain);
+        }
+
+        static void RevealAllFields()
+        {
+            for (int y = 0; y < playArea.GetLength(1); y++)
+            {
+                for (int x = 0; x < playArea.GetLength(0); x++)
+                {
+                    playArea[x, y].revealed = true;
+                }
+            }
         }
 
         static void DisplayPlayArea()
         {
-            Console.BackgroundColor = ConsoleColor.Gray;
-            Console.ForegroundColor = ConsoleColor.Black;
-            Console.Write("  ");
-            for(int width = 0; width < playArea.GetLength(0); width++)
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write(" ");
+            for (int width = 0; width < playArea.GetLength(0); width++)
             {
                 Console.Write($"{width} ");
             }
-            Console.Write("  ");
-            Console.WriteLine();
-            Console.Write("  ");
-            for (int width = 0; width < playArea.GetLength(0); width++)
-            {
-                Console.Write($"__");
-            }
-            Console.Write("  ");
+
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+
             Console.WriteLine();
 
             for (int y = 0; y < playArea.GetLength(1); y++)
             {
-                Console.Write($"{y}|");
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write($"{y}");
+
+
+
+                Console.BackgroundColor = ConsoleColor.Gray;
+                Console.ForegroundColor = ConsoleColor.Black;
+
                 for (int x = 0; x < playArea.GetLength(0); x++)
                 {
-                    if(!playArea[x, y].revealed)
+                    if (!playArea[x, y].revealed)
                     {
                         Console.BackgroundColor = ConsoleColor.Green;
-                        Console.Write("  ");
+                        if(playArea[x, y].flag)
+                        {
+                            Console.Write("f ");
+                        }else
+                        {
+                            Console.Write("  ");
+                        }
+                        
                         Console.BackgroundColor = ConsoleColor.Gray;
                         continue;
-                    } 
 
-                    if (!playArea[x, y].containsMine)
-                    {
-
-
-                        
-                            switch (playArea[x, y].adjacentMines)
-                            {
-                            case 0: break;
-                                case 1: Console.ForegroundColor = ConsoleColor.Blue; break;
-                                case 2: Console.ForegroundColor = ConsoleColor.Green; break;
-                                case 3: Console.ForegroundColor = ConsoleColor.Red; break;
-                                case 4: Console.ForegroundColor = ConsoleColor.Magenta; break;
-                                case 5: Console.ForegroundColor = ConsoleColor.Yellow; break;
-                                default: Console.ForegroundColor = ConsoleColor.Magenta; break;
-                            }
-                            Console.Write($"{playArea[x, y].adjacentMines} ");
-                            Console.ForegroundColor = ConsoleColor.Black;
-                        
                     }
+
                     if (playArea[x, y].containsMine)
                     {
                         Console.Write("X ");
+                        continue;
+                    }
+
+                    if (playArea[x, y].adjacentMines == 0)
+                    {
+                        Console.Write("  ");
+                    }
+                    else if (!playArea[x, y].containsMine)
+                    {
+                        switch (playArea[x, y].adjacentMines)
+                        {
+                            case 0: break;
+                            case 1: Console.ForegroundColor = ConsoleColor.Blue; break;
+                            case 2: Console.ForegroundColor = ConsoleColor.Green; break;
+                            case 3: Console.ForegroundColor = ConsoleColor.Red; break;
+                            case 4: Console.ForegroundColor = ConsoleColor.Magenta; break;
+                            case 5: Console.ForegroundColor = ConsoleColor.Yellow; break;
+                            default: Console.ForegroundColor = ConsoleColor.Magenta; break;
+                        }
+                        Console.Write($"{playArea[x, y].adjacentMines} ");
+                        Console.ForegroundColor = ConsoleColor.Black;
+
                     }
 
                     //Console.Write(playArea[x, y]);
                 }
-                Console.WriteLine("|");
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.WriteLine();
             }
-            Console.Write(" |");
-            for (int width = 0; width < playArea.GetLength(0); width++)
-            {
-                Console.Write($"__");
-            }
-            Console.WriteLine("|");
+
 
             Console.BackgroundColor = ConsoleColor.Black;
             Console.ForegroundColor = ConsoleColor.White;
         }
 
 
-
+        static void CheckIfWon()
+        {
+            win = true;
+            for (int y = 0; y < playArea.GetLength(1); y++)
+            {
+                for (int x = 0; x < playArea.GetLength(0); x++)
+                {
+                    if (!playArea[x, y].containsMine && !playArea[x, y].revealed)
+                    {
+                        win = false;
+                    }
+                }
+            }
+        }
 
         static int Validate_Input(int min_number, int max_number)
         {
@@ -215,6 +326,86 @@
                     playArea[x, y].adjacentMines = adjacentMines;
 
                 }
+            }
+
+        }
+
+
+        static void RevealField(int x, int y)
+        {
+            if (playArea[x, y].flag)
+            {
+                return;
+            }
+
+            playArea[x, y].revealed = true;
+
+
+
+            if (playArea[x, y].containsMine)
+            {
+                lose = true;
+                return;
+            }
+            if (playArea[x, y].adjacentMines > 0)
+            {
+                return;
+            }
+
+
+            int upperOffset = 1;
+            int lowerOffset = 1;
+            int leftOffset = 1;
+            int rightOffset = 1;
+
+            if (y == 0)
+            {
+                upperOffset = 0;
+            }
+            if (y == playArea.GetLength(1) - 1)
+            {
+                lowerOffset = 0;
+            }
+            if (x == 0)
+            {
+                leftOffset = 0;
+            }
+            if (x == playArea.GetLength(0) - 1)
+            {
+                rightOffset = 0;
+            }
+
+
+
+            for (int height = y - upperOffset; height <= y + lowerOffset; height++)
+            {
+                for (int width = x - leftOffset; width <= x + rightOffset; width++)
+                {
+
+                    if (!playArea[width, height].revealed && playArea[width, height].adjacentMines == 0)
+                    {
+                        RevealField(width, height);
+                    }
+
+                    playArea[width, height].revealed = true;
+                }
+            }
+        }
+
+        static void PlaceFlag(int x, int y)
+        {
+            if (playArea[x, y].revealed)
+            {
+                Console.WriteLine("cannot flag revealed field");
+                return;
+            }
+            if (playArea[x, y].flag)
+            {
+                playArea[x, y].flag = false;
+            }
+            else
+            {
+                playArea[x, y].flag = true;
             }
 
         }
--- minesweeper_le_first/obj/Debug/net8.0/minesweeper_le_first.AssemblyInfo.cs
+++ minesweeper_le_first/obj/Debug/net8.0/minesweeper_le_first.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("minesweeper_le_first")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cd6d158aad0a7d2200c49b11d6010318a6cdb93")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a948f4ce7dc637dbebdeab08184bca15031b965b")]
 [assembly: System.Reflection.AssemblyProductAttribute("minesweeper_le_first")]
 [assembly: System.Reflection.AssemblyTitleAttribute("minesweeper_le_first")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
